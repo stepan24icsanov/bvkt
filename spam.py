f0382ae0abc34dcb6f6288c614d3b69fccc539df
stepan24icsanov <57861034+stepan24icsanov@users.noreply.github.com>
2019-12-15 18:52:29 +0600

    Delete spam.py

--- spam.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-
-token = ''
-params = {'access_token': token, 'random_id': 0, 'peer_id'; 431149734, 'message': 'huy'}
-request = requests.get('https://api.vk.com/method/messages.send',params=params)
